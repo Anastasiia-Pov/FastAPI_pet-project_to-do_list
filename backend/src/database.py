de4dae7e918c612849f33927cff62668ce24ae4e
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-07-21 15:38:04 +0500

    add session_maker

--- backend/src/database.py
+++ backend/src/database.py
@@ -11,9 +11,16 @@ Base = declarative_base()
 
 
 engine = create_async_engine(DATABASE_URL)
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = sessionmaker(engine,
+                                   class_=AsyncSession,
+                                   expire_on_commit=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
