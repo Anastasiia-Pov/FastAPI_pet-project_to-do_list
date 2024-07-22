843409b75fa7d1220eedee7df24d212b72086685
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-07-22 17:17:53 +0500

    middleware methods & headers added

--- backend/src/main.py
+++ backend/src/main.py
@@ -33,8 +33,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie",
+                   "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
 
 
