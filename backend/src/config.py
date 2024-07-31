0fa052f526a88f2c399945dc5dd26c0b8c102eb1
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-07-31 19:42:50 +0500

    updated

--- backend/src/config.py
+++ backend/src/config.py
@@ -9,7 +9,7 @@ DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
-SECRET = os.environ.get("SECRET")
+SECRET_AUTH = os.environ.get("SECRET_AUTH")
 SECRET_VERIF = os.environ.get("SECRET_VERIF")
 SECRET_RESET = os.environ.get('SECRET_RESET')
 
@@ -19,8 +19,3 @@ DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
 DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
 DB_USER_TEST = os.environ.get("DB_USER_TEST")
 DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
-
-SECRET_AUTH = os.environ.get("SECRET_AUTH")
-
-SMTP_USER = os.environ.get("SMTP_USER")
-SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
