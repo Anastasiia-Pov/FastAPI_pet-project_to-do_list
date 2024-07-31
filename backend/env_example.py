a0a1b071e60832abeb53d9444f8860f26bd626ea
Anastasiia Povolotskaia <anastasiia.povolotskaia@gmail.com>
2024-07-31 18:11:39 +0500

    example for .env

---
+++ backend/env_example.py
@@ -0,0 +1,15 @@
+DB_HOST=***********
+DB_PORT=***********
+DB_NAME==***********
+DB_USER==***********
+DB_PASS==***********
+
+SECRET_AUTH = ***********
+SECRET_RESET = ***********
+SECRET_VERIF = ***********
+
+DB_HOST_TEST=***********
+DB_PORT_TEST=***********
+DB_NAME_TEST=***********
+DB_USER_TEST=***********
+DB_PASS_TEST=***********
